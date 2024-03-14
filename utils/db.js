e7eae4b519c6545370e033c552bf242a2e4e1e25
casonbobo <118225257+casonbobo@users.noreply.github.com>
2024-03-14 11:58:40 -0500

    Changed for eslint and added an isAlive test

--- utils/db.js
+++ utils/db.js
@@ -7,48 +7,51 @@ const database = process.env.DB_DATABASE || 'files_manager';
 const url = `mongodb://${host}:${port}/${database}`;
 
 class DBClient {
-    constructor() {
-        this.client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
-    }
+  constructor() {
+    this.client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+  }
 
-    async connect() {
-        try {
-            await this.client.connect();
-            console.log("Connected to MongoDB");
-        } catch (error) {
-            console.error("Error connecting to MongoDB:", error);
-        }
+  async connect() {
+    try {
+      await this.client.connect();
+      console.log('Connected to MongoDB');
+    } catch (error) {
+      console.error('Error connecting to MongoDB:', error);
     }
+  }
 
-    async isAlive() {
-        try {
-            await this.client.db().command({ ping: 1 });
-            return true;
-        } catch (error) {
-            console.error("Error checking MongoDB connection:", error);
-            return false;
-        }
+  async isAlive() {
+    try {
+      await this.client.db().command({ ping: 1 });
+      return true;
+    } catch (error) {
+      if (error.message.includes('not authorized on admin to execute command { ping: 1 }')) {
+        return false;
+      }
+      console.error('Error checking MongoDB connection:', error);
+      return false;
     }
+  }
 
-    async nbUsers() {
-        try {
-            const collection = this.client.db().collection('users');
-            return await collection.countDocuments();
-        } catch (error) {
-            console.error("Error counting users:", error);
-            return 0;
-        }
+  async nbUsers() {
+    try {
+      const collection = this.client.db().collection('users');
+      return await collection.countDocuments();
+    } catch (error) {
+      console.error('Error counting users:', error);
+      return 0;
     }
+  }
 
-    async nbFiles() {
-        try {
-            const collection = this.client.db().collection('files');
-            return await collection.countDocuments();
-        } catch (error) {
-            console.error("Error counting files:", error);
-            return 0;
-        }
+  async nbFiles() {
+    try {
+      const collection = this.client.db().collection('files');
+      return await collection.countDocuments();
+    } catch (error) {
+      console.error('Error counting files:', error);
+      return 0;
     }
+  }
 }
 
 const dbClient = new DBClient();
--- utils/redis.js
+++ utils/redis.js
@@ -2,32 +2,32 @@ const redis = require('redis');
 const { promisify } = require('util');
 
 class RedisClient {
-    constructor() {
-        this.client = redis.createClient({
+  constructor() {
+    this.client = redis.createClient({
     });
 
-        this.client.on('error', (err) => console.error(err));
+    this.client.on('error', (err) => console.error(err));
 
-        this.getAsync = promisify(this.client.get).bind(this.client);
-        this.setAsync = promisify(this.client.set).bind(this.client);
-        this.delAsync = promisify(this.client.del).bind(this.client);
-    }
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
+  }
 
-    isAlive() {
-        return this.client.connected;
-    }
+  isAlive() {
+    return this.client.connected;
+  }
 
-    async get(key) {
-        return this.getAsync(key);
-    }
+  async get(key) {
+    return this.getAsync(key);
+  }
 
-    async set(key, value, duration) {
-        return this.setAsync(key, value, 'EX', duration);
-    }
+  async set(key, value, duration) {
+    return this.setAsync(key, value, 'EX', duration);
+  }
 
-    async del(key) {
-        return this.delAsync(key);
-    }
+  async del(key) {
+    return this.delAsync(key);
+  }
 }
 
 const redisClient = new RedisClient();
