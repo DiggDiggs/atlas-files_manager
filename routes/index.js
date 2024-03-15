d3a3efc51dab4b7a7482c176f0daabb919129360
DiggDiggs <christopherdiggs1@gmail.com>
2024-03-15 12:40:19 -0500

    usercontroller also added in index

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,15 @@ const express = require('express');
 
 // controllers
 const AppController = require('../controllers/AppController');
+const AuthController = require('../controllers/AuthController');
 const router = express.Router();
 
 // get routes
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+
+// post routes
 
 module.exports = router;
