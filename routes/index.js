09a5f5105b82945e7ea26bdc4017162749c4c521
DiggDiggs <christopherdiggs1@gmail.com>
2024-03-19 01:19:11 -0500

    task 5 routes setup

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+import FilesController from('../controllers/FilesController');
 
 // Get Routes
 router.get('/status', AppController.getStatus);
@@ -11,8 +12,12 @@ router.get('/stats', AppController.getStats);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
+router.get('/files', FilesController.getIndex);
+router.get('/files/:id/data', FilesController.getFile);
 
 // Post Routes
 router.post('/users', UsersController.postNew);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
 
 module.exports = router;
